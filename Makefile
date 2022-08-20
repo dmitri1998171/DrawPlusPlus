6b6a69d9e4c44ffbea734bc132ecbef31ddd05b1
dmitri1998171 <dmitri199817@gmail.com>
2022-08-20 15:28:09 +0700

    rewrite draw curve line feature

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 BIN := main
+TEST := test
 SRC := main.cpp
+SRC_TEST := test.cpp
 CC := g++
 LIB_PATH := -L/System/Library/Frameworks
 LDFLAGS := -framework GLUT -framework OpenGL -framework Cocoa
@@ -7,6 +9,9 @@ CFLAGS := -Wno-deprecated-declarations
 
 all:
 	clear && $(CC) $(SRC) $(LIB_PATH) $(LDFLAGS) $(CFLAGS) -o $(BIN)
+
+test:
+	clear && $(CC) $(SRC_TEST) $(LIB_PATH) $(LDFLAGS) $(CFLAGS) -o $(TEST)
 
 clean:
 	rm -rf $(BIN)
--- main.cpp
+++ main.cpp
@@ -1,31 +1,43 @@
-// #include <GL/glut.h>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include "Menu.h"
 
+#define WIDTH 500
+#define HEIGHT 500
+
 using namespace std;
 
-int I = 10000;
-bool down = false;
-
 void renderScene(void) {
+	glFlush();
+	glutPostRedisplay();
+}
+
+void mouseMove(int x, int y) {
 	glPointSize(point_size);
 	glColor3f(red, green, blue);
 	// glEnable(GL_LINE_STIPPLE); 		// разрешаем рисовать прерывистую линию
 	// glLineStipple(2, 58360);    		// устанавливаем маску
 	
+	y = WIDTH - y;
+
 	if(line_type == 1){
-		glBegin(GL_LINES);
+		glBegin(GL_LINE_STRIP);
 		glVertex2f(xx-1, -yy+1);
 		glVertex2f(X-1, -Y+1);
 		glEnd();
 	}
 
 	if(line_type == 2){
-		glBegin(GL_POINTS);
-		glVertex2f(X-1, -Y+1);
-		glEnd();
+		glBegin(GL_LINES);
+            glVertex2f(xx, yy);
+            glVertex2f(x, y);
+        glEnd();
+
+        glFlush();
+
+        xx = x;
+        yy = y;
 	}
 
 	if(line_type == 3){
@@ -44,65 +56,25 @@ void renderScene(void) {
 		glVertex2f(X-1, -Y+1);
 		glEnd();
 	}
-
-    glutSwapBuffers();
-}
-
-int frame=0,t,timebase=0,fps;
-
-void mouseMove(int x, int y) {
-
-	X = x; Y = y;
-	X = X / _count; Y = Y / _count;		
-
-
-	if(check == 1) xx=X; yy=Y;
-	if(check == 2) check = 0;
-	check++;
-	
-	frame++;
-	t=glutGet(GLUT_ELAPSED_TIME);
-	if (t - timebase > 1000) {
-		fps = frame*1000.0/(t-timebase);
-	 	timebase = t;
-		frame = 0;
-	}
-
-	// cout <<"X: " << X << " Y: " << Y;
-	// cout <<" xx: " << xx << " yy: " << yy;
-	// cout << " check: " << check << " fps:" << fps << endl;
 }
 
 void MouseFunc(int button, int state, int x, int y) {
-	if(button == GLUT_LEFT_BUTTON)
-		if(state == GLUT_DOWN)
-			{
-				down = true;
-			}
-		if(state == GLUT_UP)
-			{
-				down = false;
-			}
-
-	//I = 0;	
+	y = WIDTH - y;
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+		xx=x; yy=y;
 }
 
 void keyboardFunc(unsigned char key, int x, int y){
 	if(key == 27) exit(0);
 }
 
-void timf(int value) {
-  glutPostRedisplay();  // Redraw windows
-  glutTimerFunc(millis, timf, 0); // Setup next timer
-}
-
 int main(int argc, char **argv) {
  
 	// инициализация
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(500,500);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WIDTH, HEIGHT);
 	glutCreateWindow("Draw++");
  
 	glClearColor (1.0, 1.0, 1.0, 1.0);
@@ -110,33 +82,15 @@ int main(int argc, char **argv) {
 
 	// регистрация обратных вызовов
 	glutDisplayFunc(renderScene);
-	glutTimerFunc(millis, timf, 0);
 	glutKeyboardFunc(keyboardFunc);
 	createPopupMenus();
 	glutMouseFunc(MouseFunc);
 	glutMotionFunc(mouseMove);
+
+	gluOrtho2D(0,WIDTH,0,HEIGHT);
 
 	// Основной цикл GLUT
 	glutMainLoop();
  
 	return 1;
 }
-
-/*
-	TO DO
-
-\/ 1. Отрисовка
-2. Меню
-	\/ - цвет
-	\/ - толщина
-	\/ - тип (прямые, зеркальные, кривые)
-	\/ - пунктир
-	\/ - фон
-	\/ - очистка
-	- ластик
-\/ 3. смена фона
-4. оптимизация
-	- изменение размеров окна
-5. тип (прямые, зеркальные, кривые)
-
-*/
